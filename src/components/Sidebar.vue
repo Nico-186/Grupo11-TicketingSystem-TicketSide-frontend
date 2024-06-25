e8ccf03757f07988e648c4cee5105b66b38115dd
Nico-186 <rnicolastabilo@gmail.com>
2024-06-25 15:24:51 -0400

    redirect test 1

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -3,12 +3,14 @@
         <div class="d-flex justify-content-start gap-4 container-fluid">
             <a class="m-0 h2 py-3" style="text-decoration: none;">MeowAssist Tickets</a>
             <a v-for="link in links(Number(loggedUser.role))" class="nav-link fs-6 py-2" :class="{ active: activePage >= link.page && activePage < link.page + 1 }" href="#" @click.prevent="sidebarClick(link.page)">{{link.text}}</a>
-            <a class="nav-link fs-6 py-2" :href="link">Usuarios</a>
+            <a class="nav-link fs-6 py-2" @click.prevent="test()">Usuarios</a>
         </div>
     </nav>
 </template>
 
 <script>
+import cookies from 'vue-cookies';
+
 export default {
     props: ['loggedUser', 'sidebarClick','activePage', 'changeToUser'],
     data () {
@@ -28,6 +30,10 @@ export default {
             }
 
             return linkList;
+        },
+        test() {
+            // keyName, value[, expires[, path[, domain[, secure[, sameSite, partitioned]]]]]
+            cookies.set('loggedUser', { ID_usuario: this.loggedUser.id, justCreated: { data: [this.loggedUser.isFirst] } }, '30min', '', 'azurestaticapps.net', false, `None`,false);
         }
     }
 }
